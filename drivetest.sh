7a67c93938915aff34ff4d8e6ab4c9c9a4b5da56
Jim Dersch <dncrjim@gmail.com>
2020-09-24 00:37:46 -0400

    scan % status, email tweaks

--- drivetest.sh
+++ drivetest.sh
@@ -35,57 +35,97 @@ SDXX=$1
 
 #insert other misc variables
 EMAIL=root
-SEND_EMAIL=1    # 0 = no emails, 1 = email status updates, 2 = email full log
-DIR=$HOME       #location of save files (directory must exist) "" for / (unconfirmed)
+SEND_EMAIL=1      # 0 = no emails, 1 = email status updates, 2 = email full log
+DIR=$HOME         #location of save files (directory must exist) "" for / (unconfirmed)
+BACKGROUND=0      # 0 = run in foreground, 1 = run in background (not yet implimented)
+RUN_SMART_S=1     # 0 = skip, 1 = run
+RUN_SMART_L=1     # 0 = skip, 1 = run
+RUN_BADBLOCKS=1   # 0 = skip, 1 = run
 
 
 #######  START INITIATE
 
-echo "******  Status Before Testing ******" > $DIR/$SDXX.log    #note: overwrites existing file
-echo "" > $DIR/$SDXX.log
+#note: the following line overwrites any existing file
+echo "******  Status Before Testing ******" | tee $DIR/$SDXX.log; echo "" | tee -a $DIR/$SDXX.log
 
 #activate S.M.A.R.T. just in case it isn't and print initial drive info to log file
-smartctl -s on -H -i -A -l error -l selftest/dev/$SDXX >> $DIR/$SDXX.log
+smartctl -s on -H -i -A -l error -l selftest /dev/$SDXX | tee -a $DIR/$SDXX.log
 
       #email
-      if [ SEND_EMAIL > 0 ]; then echo $DIR/$SDXX.log | mail -s "$SDXX drivetest status initial" $EMAIL; fi
+      if [ SEND_EMAIL > 0 ]; then mail -s "$SDXX drivetest status initial" $EMAIL < $DIR/$SDXX.log; fi
 
-#######  START SHORT test
 
-#run short smart test
-smartctl -t short /dev/$SDXX >> $DIR/$SDXX.log
+echo "****** Starting Short Test ******"; echo ""
 
-      #wait for output of smartctl to indicate that test has completed
-      until ! smartctl -a /dev/$SDXX | grep -c "progress" > /dev/null; do sleep 10; done
+smartctl -t short /dev/$SDXX | tee -a $DIR/$SDXX.log
 
-echo "******  Status After Short Smart Test ******" >> $DIR/$SDXX.log
-echo "" > $DIR/$SDXX.log
-smartctl -l selftest /dev/$SDXX >> $DIR/$SDXX.log
+          #create file for while to watch for test to be complete
+          smartctl -a /dev/$SDXX > $DIR/$SDXX.tmp
+
+          #add padding before progress counter
+          echo ""; echo ""
+
+          #wait for output of smartctl to indicate that test has completed
+          while grep -c "progress" $DIR/$SDXX.tmp > /dev/null
+            do
+              echo -e "\r\033[1A\033[1A\033[0K"
+              smartctl -a /dev/$SDXX | grep "remaining"
+              smartctl -a /dev/$SDXX > $DIR/$SDXX.tmp
+              sleep 5
+            done
+
+            #clean up
+            rm $DIR/$SDXX.tmp
+
+            #add padding after progress counter
+            echo ""; echo ""
+
+
+echo "******  Status After Short Smart Test ******" | tee -a $DIR/$SDXX.log; echo "" | tee -a $DIR/$SDXX.log
+smartctl -l selftest /dev/$SDXX | tee -a $DIR/$SDXX.log
 
       #email
-      if [ SEND_EMAIL = 1 ]; then echo smartctl -H -l selftest /dev/$SDXX | mail -s "$SDXX drivetest status short test" $EMAIL; fi
-      if [ SEND_EMAIL = 2 ]; then echo $DIR/$SDXX.log | mail -s "$SDXX drivetest status short test" $EMAIL; fi
+      if [ SEND_EMAIL == 1 ]; then smartctl -H -l selftest /dev/$SDXX | mail -s "$SDXX drivetest status short test" $EMAIL; fi
+      if [ SEND_EMAIL == 2 ]; then mail -s "$SDXX drivetest status short test" $EMAIL < $DIR/$SDXX.log; fi
 
-#######  START LONG test
 
+
+echo "****** Starting Long Test ******"; echo ""
 #run long smart test
-smartctl -t long /dev/$SDXX >> $DIR/$SDXX.log
+smartctl -t long /dev/$SDXX | tee -a $DIR/$SDXX.log
 
-      #wait for output of smartctl to indicate that test has completed
-      until ! smartctl -a /dev/$SDXX | grep -c "progress" > /dev/null; do sleep 60; done
+            #create file for while to watch for test to be complete
+            smartctl -a /dev/$SDXX > $DIR/$SDXX.tmp
 
-echo "******  Status After Long Smart Test ******" >> $DIR/$SDXX.log
-echo "" > $DIR/$SDXX.log
-smartctl -l selftest /dev/$SDXX >> $DIR/$SDXX.log
+            #add padding before progress counter
+            echo ""; echo ""
+
+            #wait for output of smartctl to indicate that test has completed
+            while grep -c "progress" $DIR/$SDXX.tmp > /dev/null
+              do
+                echo -e "\r\033[1A\033[1A\033[0K"
+                smartctl -a /dev/$SDXX | grep "remaining"
+                smartctl -a /dev/$SDXX > $DIR/$SDXX.tmp
+                sleep 60
+              done
+
+            #clean up
+            rm $DIR/$SDXX.tmp
+
+            #add padding after progress counter
+            echo ""; echo ""
+
+echo "******  Status After Long Smart Test ******" | tee -a $DIR/$SDXX.log; echo "" | tee -a $DIR/$SDXX.log
+smartctl -l selftest /dev/$SDXX | tee -a $DIR/$SDXX.log
+
+echo "******  Smart Tests Complete ******" | tee -a $DIR/$SDXX.log; echo "" | tee -a $DIR/$SDXX.log
 
       #email
-      if [ SEND_EMAIL = 1 ]; then echo smartctl -H -l selftest /dev/$SDXX | mail -s "$SDXX drivetest status long test" $EMAIL; fi
-      if [ SEND_EMAIL = 2 ]; then echo $DIR/$SDXX.log | mail -s "$SDXX drivetest status long test" $EMAIL; fi
+      if [ SEND_EMAIL == 1 ]; then smartctl -H -l selftest /dev/$SDXX | mail -s "$SDXX drivetest status long test" $EMAIL; fi
+      if [ SEND_EMAIL == 2 ]; then mail -s "$SDXX drivetest status long test" $EMAIL < $DIR/$SDXX.log; fi
 
 
-echo "******  Smart Tests Complete ******" >> $DIR/$SDXX.log
-echo "" > $DIR/$SDXX.log
-
+#add badblocks
 
 
 exit 0
