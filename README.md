9ac20bb6f93eba9ca4cb128f8e9dec7cd1458243
Jim Dersch <dncrjim@gmail.com>
2020-10-04 15:01:05 -0400

    update and set up for hacktoberfest

--- README.md
+++ README.md
@@ -1,35 +1,37 @@
 # disktest
 script for testing incoming sata/sas drives
 
-##Testing Sequence (checks indicate completed sections)
-[*] smart test - short
-[*] smart test - Long
-[*] badblocks - standard 4 pass r/w
-[ ] test/report drive r/w speed
+## Testing Sequence (checks indicate completed sections)
+[x] smart test - short
+[x] smart test - Long
+[x] badblocks - standard 4 pass r/w
+[x] test/report drive r/w speed
 [ ] zfs r/w test under compression
   [ ] create single disk zfs pool w/ compression
   [ ] run standard single pass r/w test
   [ ] destroy zfs pool
-
-## Dependencies
-* badblocks (e2fsprogs)
-* smartctl (smartmontools)
-* zfs
+[ ] run short/long smart test again at end?
+  [ ] create flag for this?
 
 ## Possible Future Development
 [ ] insert time stamps in log
 [ ] update "**skipping" and "**starting" inserts to make sure they log correctly
-[ ] confirm r/w test is destructive
-  [ ] add optional r/w test for when drive is in use
+[ ] confirm if r/w speed test is destructive
+  [ ] optional: add r/w test for when drive is in use?
 [ ] add confirmation messages at beginning of script
   [ ] confirm device name and if badblocks/zfs is selected, confirm data erasure
 [ ] set up to run program in background
 [ ] allow input of a list of multiple drives
 [ ] flags for automatic enable/disable of individual tests
-  [ ] -y flag to automatically agree to all tests
-  [ ] -a for all tests (-slbz)
+  [ ] -b flag for run in background
   [ ] -z flag for zfs
 
-## Other Notes:
+### Dependencies
+  * smartctl (smartmontools)
+  * badblocks (e2fsprogs)     .only applies to badblocks test
+  * zfs                       .only applies to zfs test
+  * f3                        .only applies to zfs test
+
+### Other Notes
 designed for sata/sas disks only, ATA info reporting is not enabled
 must be run as root
--- disktest.sh
+++ disktest.sh
@@ -14,31 +14,32 @@ if [[ "sd" != "${1:0:2}" ]] ; then echo "incorrect syntax" ; echo "usage: diskte
 SDXX=$1
 
 #insert other variables (selections may be overwritten by flags below)
-EMAIL="root"
-SEND_EMAIL=1      # 0 = no emails, 1 = email status updates, 2 = email full log each time
-DIR=""            #location of save files (directory must exist), $HOME will be root home, "" for / (unconfirmed)
+ERASE_IT_ALL="n"  #sets erase to no
+DIR=""            #location of save files (directory must exist), $HOME will be root home (I think?), "" for / (unconfirmed)
 BACKGROUND=0      # 0 = run in foreground, 1 = run in background (not yet implimented)
 RUN_SMART_S=0     # 1 = run
 RUN_SMART_L=0     # 1 = run
 RUN_BADBLOCKS=0   # 1 = run
 RUN_SPEED_TEST=0  # 1 = run
 RUN_ZFS_TEST=0    # 1 = run
+SEND_EMAIL=1      # 0 = no emails, 1 = email status updates, 2 = email full log each time
+EMAIL="root"
 
 #import flags and save to variables
 while getopts ybaslbwzm:e: option
 do
   case "${option}"
     in
-    y) ERASE_IT_ALL="y";;
+    y) ERASE_IT_ALL="y" ;;
     b) echo "you've used an unavailable feature; goodbye." ; exit 1 ;;
     a) RUN_ALL=1;;
-    s) RUN_SMART_S=1;;
-    l) RUN_SMART_L=1;;
-    b) RUN_BADBLOCKS=1;;
-    w) RUN_SPEED_TEST=1;;
+    s) RUN_SMART_S=1 ;;
+    l) RUN_SMART_L=1 ;;
+    b) RUN_BADBLOCKS=1 ;;
+    w) RUN_SPEED_TEST=1 ;;
     z) echo "you've used an unavailable feature; goodbye." ; exit 1 ;;
-    m) SEND_EMAIL=${OPTARG};;
-    e) EMAIL=${OPTARG};;
+    m) SEND_EMAIL=${OPTARG} ;;
+    e) EMAIL=${OPTARG} ;;
   esac
 done
 
@@ -58,14 +59,13 @@ if [[ RUN_BADBLOCKS == 1 ]] || [[ RUN_SPEED_TEST == 1 ]] || [[ RUN_ZFS_TEST == 1
       do
         echo -n "Are you sure you are willing to lose any/all data on /dev/$SDXX? (y/n): "
         read ERASE_IT_ALL
-          if [[ $CONTINUE == "n" ]] ; then echo "goodbye." ; exit 1 
-          elif [[ $CONTINUE != "y" ]] ; then echo "I don't understand your response.."
+          if [[ $ERASE_IT_ALL == "n" ]] ; then echo "goodbye." ; exit 1
+          elif [[ $ERASE_IT_ALL != "y" ]] ; then echo "I don't understand your response.."
           fi
       done
   fi
 
-#insert code to run in background if background = 0
-
+#insert code to run in background if BACKGROUND = 1
 
 ######################################
 #######  MAIN BODY OF TESTING
@@ -176,15 +176,11 @@ if [[ $RUN_BADBLOCKS == 1 ]]
           if [ SEND_EMAIL == 1 ]; then smartctl -H -l selftest /dev/$SDXX | mail -s "$SDXX disktest status after badblocks" $EMAIL; fi
           if [ SEND_EMAIL == 2 ]; then mail -s "$SDXX disktest status after badblocks" $EMAIL < $DIR/$SDXX.log; fi
 
-
-
-
     else echo "****** Skipping Badblocks ******" |& tee -a $DIR/$SDXX.log; echo "" |& tee -a $DIR/$SDXX.log
 fi
 
 if [[ $RUN_SPEED_TEST == 1 ]]
     then echo "****** Starting r/w Speed Test ******" |& tee -a $DIR/$SDXX.log
-        echo "****** Starting r/w Speed Test ******" |& tee $DIR/$SDXX.tmp
         echo "" |& tee -a $DIR/$SDXX.log $DIR/$SDXX.tmp
 
     echo "1G file size speed test" |& tee -a $DIR/$SDXX.log $DIR/$SDXX.tmp
@@ -208,6 +204,49 @@ if [[ $RUN_SPEED_TEST == 1 ]]
     rm $DIR/$SDXX.tmp   #clean up
     else echo "****** Skipping r/w Speed Test ******" |& tee -a $DIR/$SDXX.log; echo "" |& tee -a $DIR/$SDXX.log
 fi
+
+
+# if [[ $RUN_ZFS_TEST == 1 ]]
+#     then echo "****** Starting ZFS Compression Test ******" |& tee -a $DIR/$SDXX.log
+#         echo "" |& tee -a $DIR/$SDXX.log $DIR/$SDXX.tmp
+#
+########################## #zfs script goes here
+#  outline:
+#       create single disk zfs pool w/ compression
+#       run standard single pass r/w test
+#       destroy zfs pool
+#
+#
+# the below should be able to be reconfigured/written to impliment the zfs test
+#
+#                 sudo parted /dev/sdxx mklabel gpt
+#                 sudo parted -a opt /dev/sdxx mkpart primary 0% 100%
+#
+#                 zpool create -f -o ashift=12 -O logbias=throughput -O compress=lz4 -O dedup=off -O atime=off -O xattr=sa <unique_pool_name> /dev/sdxx
+#                 zpool export <unique_pool_name>
+#                 sudo zpool import -d /dev/disk/by-id <unique_pool_name>
+#                 sudo chmod -R ugo+rw /<unique_pool_name>
+#
+#                 fill test
+#                 f3write /<unique_pool_name> && f3read /<unique_pool_name>
+#                 zpool scrub <unique_pool_name>  #this obviously has to be confirmed that it outputs results (tmp file)
+#
+#                 #nuke drive:
+#
+#                 dd if=/dev/zero of=/dev/sdXX bs=512 count=10  #clear first sector
+#                 dd if=/dev/zero of=/dev/sdXX bs=512 seek=$(( $(blockdev --getsz /dev/sdXX) - 4096 )) count=1M  #clear last sector?
+#
+# #
+#
+#         if [ SEND_EMAIL == 1 ]; then mail -s "$SDXX disktest status after zfs test" $EMAIL < $DIR/$SDXX.tmp; fi
+#         if [ SEND_EMAIL == 2 ]; then mail -s "$SDXX disktest status after zfs test" $EMAIL < $DIR/$SDXX.log; fi
+#
+#     rm $DIR/$SDXX.tmp   #clean up
+#
+#     else echo "****** Skipping ZFS Compression Test ******" |& tee -a $DIR/$SDXX.log; echo "" |& tee -a $DIR/$SDXX.log
+# fi
+
+
 
 echo "" |& tee -a $DIR/$SDXX.log; echo "" |& tee -a $DIR/$SDXX.log   #add padding
 echo  "****** End of Testing ******" |& tee -a $DIR/$SDXX.log
