4bdd5b09313623859589185d59bcd3f6efa8127a
ssfalkster <76265276+ssfalkster@users.noreply.github.com>
2021-01-02 19:56:59 -0500

    Update disktest.sh
    I'm dumb

--- disktest.sh
+++ disktest.sh
@@ -72,9 +72,8 @@ if [[ $RUN_BADBLOCKS == 1 ]] || [[ $RUN_SPEED_TEST == 1 ]] || [[ $RUN_ZFS_TEST =
 
 #insert code to stop testing if no tests have been selected
 
-if [[ $RUN_ALL=0 && RUN_SMART_S=1 && $RUN_SMART_L=0 && $RUN_BADBLOCKS=0 && $RUN_SPEED_TEST=0 && RUN_ZFS_TEST=0 ]]
+if [[ $RUN_ALL=0 && RUN_SMART_S=0 && $RUN_SMART_L=0 && $RUN_BADBLOCKS=0 && $RUN_SPEED_TEST=0 && RUN_ZFS_TEST=0 ]]
   then
-    do
      echo "You have not selected a test to run on /dev/$SDXX?" ; then echo "goodbye." ; exit 1
      done
  fi
